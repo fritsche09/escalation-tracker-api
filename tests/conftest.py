eaf09909251595a9f4beea31cd6d7017e9c3842d
Erick Fritsche <frozenerick1@gmail.com>
2026-04-19 00:55:06 -0400

    Create test_user and test_token fixtures in tests/conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from passlib.context import CryptContext
 from app.main import app
 from app.database import Base, get_db
 import pytest
+from app.models.user import User
+
 
 TEST_DATABASE_URL = "postgresql://localhost/escalation_tracker_test"
 
@@ -11,6 +14,7 @@ TEST_DATABASE_URL = "postgresql://localhost/escalation_tracker_test"
 engine=create_engine(TEST_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @pytest.fixture
 def db_session():
@@ -33,3 +37,29 @@ def client(db_session):
 
     yield TestClient(app)
     app.dependency_overrides.clear()
+
+@pytest.fixture
+def test_user(db_session):
+    new_user = User(
+        name = "test_user",
+        email = "test@123.com",
+        hashed_password = pwd_context.hash("test_password")
+    )
+
+    db_session.add(new_user)
+    db_session.commit()
+    db_session.refresh(new_user)
+
+    return new_user
+
+@pytest.fixture
+def test_token(client, test_user):
+    response = client.post("/auth/login", data={
+        "username": test_user.email, 
+        "password": "test_password"
+    })
+
+    token = response.json()["access_token"]
+
+    return token
+
